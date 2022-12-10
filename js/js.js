d68c06e3c2f9393dd25912bc0df9d2f759f8332c
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2022-12-10 22:37:59 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -26,55 +26,78 @@
 //   { id: "b", parentId: "a" },
 //   { id: "c", parentId: "a" },
 //   { id: "d", parentId: null },
-//   { id: "e", parentId: "e" },
+//   { id: "e", parentId: "d" },
 // ];
 
 const arr = [
   { id: "a", parentId: null },
   { id: "b", parentId: "a" },
   ,
-  { id: "q", parentId: "b" },
   { id: "c", parentId: "a" },
   { id: "d", parentId: null },
   { id: "e", parentId: "e" },
+  { id: "f", parentId: "b" },
+  { id: "q", parentId: "b" },
+  { id: "h", parentId: "e" },
 ];
 
-// const newObg = {};
-
-// function SortArr(arr = []) {
-//   arr.map(({ id, parentId }) => {
-//     newObg[id] = { id, parentId };
-//     arr
-//       .filter((u) => id === u.parentId)
-//       .forEach((line) => {
-//         let ident = line.id;
-//         newObg[id][ident] = line;
-//       });
-//   });
-//   console.log(newObg);
-// }
-
-// SortArr(arr);
+const newObj = {};
+const newArr = [];
 
 function SortArr(arr = []) {
-  const newObj = {};
-  arr.map(({ id, parentId }) => {
-    newObj[id] = { id, parentId };
-    arr
-      .filter((u) => id === u.parentId)
-      .forEach((line) => {
-        let ident = line.id;
-        newObj[id][ident] = line;
-      });
-  });
-  console.log(newObj);
-  // const entries = Object.entries(newObj);
-  // const keys = Object.keys(newObj)
-  // for (key of keys) {
-  //   for (entrie of entries) {
-  //   entrie.include(key) ?
-  //   }
-  // }
-}
+  arr.filter(({ id, parentId }) => {
+    if (id === parentId) {
+      newObj[id] = { id, parentId, [id]: { id, parentId } };
+    }
+    if (parentId === null || id === parentId) {
+      newObj[id] = { id, parentId };
 
+      arr
+        .filter((u) => id === u.parentId)
+        .forEach((line) => {
+          let ident = line.id;
+          newObj[id][ident] = line;
+        });
+    }
+  });
+  arr.filter(({ id, parentId }) => {
+    if (!Object.keys(newObj).includes(parentId) && parentId !== null) {
+      newArr.push({ id, parentId });
+    }
+  });
+  const objValues = Object.values(newObj);
+  checking(objValues);
+  function checking(arr) {
+    arr.forEach((val) => {
+      newArr.forEach(({ id, parentId }) => {
+        if (Object.keys(val).includes(parentId)) {
+          val[parentId][id] = { id, parentId };
+          newArr.splice({ id, parentId }, 1);
+        }
+      });
+    });
+    if (newArr.length > 0) checking(arr);
+  }
+}
 SortArr(arr);
+
+console.log(newObj);
+
+// if (newArr.length !== 0) {
+//   const entries = Object.entries(newObj);
+//   newArr.map(({ id, parentId }) => {
+//     review(entries);
+//     function review(arr) {
+//       const newA = arr.map((el) => {
+//         if (Object.entries(el)[0].includes(parentId)) {
+//           el[parentId][id] = { id, parentId };
+//         } else {
+//           return;
+//         }
+//       });
+//       if (newArr.length > 0) review(newA);
+//     }
+// });
+// console.log(entries);
+// console.log(newArr);
+// }
